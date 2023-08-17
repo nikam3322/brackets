6f1b690884f66843bcdae89f1daf2907063123ab
nikam3322 <nikam3322@gmail.com>
2023-08-17 18:10:35 +0200

    changed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let arr = [];
+	let obj = {};
+	for (let i = 0; i < bracketsConfig.length; i++) {
+    		obj[bracketsConfig[i][0]] = bracketsConfig[i][1];
+	}
+	for (let i = 0; i < str.length; i++) {
+		if (obj[arr[arr.length - 1]] == str[i]) {
+			arr.pop();
+		} else {
+			arr.push(str[i]);
+		}
+	}
+	return arr == 0;
 }
